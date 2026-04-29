6d388afba168404153341e5da08b86a347f94e45
Ankush Kashyap <ankushk393939@gmail.com>
2026-04-29 16:01:16 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,redirect
 import json
+import os
 
 app = Flask(__name__)
 
@@ -72,4 +73,8 @@ def delete(index):
     return redirect('/')
 
 
-app.run(debug=True)
+
+
+if __name__ == "_main_":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
